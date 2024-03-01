a0c5b8672b785ff531d7aaab1f08d97bc3c75c03
marcodev2131 <145700074+marcodev2131@users.noreply.github.com>
2024-03-01 19:39:45 -0300

    primeiro commit

---
+++ build/scripts/script.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#carousel").slick({slidesToShow:3,slidesToScroll:1,autoplay:!0,autoplaySpeed:2e3})});
---
+++ source/scripts/script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    $('#carousel').slick({
+        // Configurações do carousel
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
