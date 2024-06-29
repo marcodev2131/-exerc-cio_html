19c8566b5a0a31106b617fc40738cee7f76616c7
marcodev2131 <145700074+marcodev2131@users.noreply.github.com>
2024-06-29 18:39:08 -0300

    corrigindo o codigo

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-// Função de multiplicação
 function multiplicar(a, b) {
     return a * b;
 }
-// Função de saudação
 function saudar(nome) {
     return "Olá " + nome;
 }
-// Exemplos de uso
 var resultadoMultiplicacao = multiplicar(5, 3);
 var saudacao = saudar("Maria");
-console.log(resultadoMultiplicacao); // Saída: 15
-console.log(saudacao); // Saída: Olá Maria
+console.log(resultadoMultiplicacao);
+console.log(saudacao);
