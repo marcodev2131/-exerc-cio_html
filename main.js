52ec3565550b15eafeb0fef8dee68efeb2b05ff1
marcodev2131 <145700074+marcodev2131@users.noreply.github.com>
2024-06-29 18:35:18 -0300

    Adicionando arquivo main.ts

---
+++ main.js
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudar(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso
+var resultadoMultiplicacao = multiplicar(5, 3);
+var saudacao = saudar("Maria");
+console.log(resultadoMultiplicacao); // Saída: 15
+console.log(saudacao); // Saída: Olá Maria
